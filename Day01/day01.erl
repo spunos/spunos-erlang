4698f06ed69a5e93545362b488a3479263950847
Sonja Dorrenboom <sonjad@kth.se>
2017-12-02 07:58:41 +0100

    task 2

--- Day01/day01.erl
+++ Day01/day01.erl
@@ -2,10 +2,21 @@
 -export([day01/0]).
 
 day01() -> {ok, Input} = file:read_file("input.txt"),
-           solve(0, hd(parse(Input)) ++ [hd(hd(parse(Input)))]).
+           List = hd(parse(Input)),
+
+           % first task
+           erlang:display(solve(0, List ++ [hd(List)])), 
+
+           % second task
+           {L1, L2} = lists:split(length(List) div 2, List),
+           solve(0, L1, L2).
 
 parse(Bin) -> string:tokens(binary_to_list(Bin), "\r\n").
 
 solve(N, [H|[H|T]]) -> solve(N + H - 48, [H|T]);
 solve(N, [_|[H|T]]) -> solve(N, [H|T]);
 solve(N, [_]) -> N.
+
+solve(N, [H|T], [H|Ts]) -> solve(N + (H - 48) * 2, T, Ts);
+solve(N, [_|T], [_|Ts]) -> solve(N, T, Ts);
+solve(N, _, _) -> N.
